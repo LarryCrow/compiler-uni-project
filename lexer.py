559febed31335f01efe3871fe6505562ab9f69c8
Dmitry <dmitri.bitkovski@gmail.com>
2019-03-02 22:09:19 +0700

    for merging

--- lexer.py
+++ lexer.py
@@ -1,1 +1,15 @@
-print("It works")
+import ply.lex as lex
+from tokens import *
+
+
+lexer = lex.lex()
+
+with open("sample.doh", 'r', encoding="UTF-8") as f:
+    data = f.read()
+    lexer.input(data)
+
+    tokens_arr = []
+    symbol_counter = 0
+
+    for lexeme in lexer:
+        print(lexeme)
